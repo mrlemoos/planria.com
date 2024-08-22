62e6a8add7a3f779b357aa5284e224334583a683
Leonardo Lemos <leo.lemos.ds@icloud.com>
2024-08-22 20:25:04 -0300

    build: remove entrypoint from design package

--- packages/design/tsup.config.ts
+++ packages/design/tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig((options) => ({
-  entryPoints: ["src/button.tsx"],
+  entryPoints: ["src"],
   format: ["cjs", "esm"],
   dts: true,
   sourcemap: true,
