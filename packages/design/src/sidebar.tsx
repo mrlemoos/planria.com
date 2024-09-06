8b9bc786604b7f1815f4286f70fa9208c40a554e
Leonardo Lemos <leo.lemos.ds@icloud.com>
2024-09-06 02:04:46 -0300

    refactor(design): use foreground colour in sidebar item label

--- packages/design/src/sidebar.tsx
+++ packages/design/src/sidebar.tsx
@@ -109,7 +109,7 @@ export function SideBar({
         {...props}
         className={cn(
           "fixed top-0 left-0 z-20 h-screen -translate-x-full lg:translate-x-0 transition-[width] ease-in-out duration-300 bg-background text-foreground",
-          isOpen ? "w-72" : "w-[90px]",
+          isOpen ? "w-60" : "w-[90px]",
           className
         )}
       >
@@ -181,7 +181,7 @@ export function SideBarItemLabel({
     <span
       {...props}
       className={cn(
-        "font-medium text-base whitespace-nowrap translate-[transform,opacity,display] ease-in-out duration-300",
+        "text-sm font-medium text-foreground whitespace-nowrap translate-[transform,opacity,display] ease-in-out duration-300",
         isOpen
           ? "translate-x-0 opacity-100"
           : "-translate-x-96 opacity-0 hidden"
