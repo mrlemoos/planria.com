fe845d3e99ce95733736b6e5a5428b59c9e91eff
Leonardo Lemos <leo.lemos.ds@icloud.com>
2024-09-07 09:45:02 -0300

    refactor: use `MiddlewareConfig` from next to assigning `config` constant

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -1,6 +1,8 @@
+import type { MiddlewareConfig } from "next/server";
+
 import { createAuthMiddleware } from "$/lib/auth/server";
 
-export const config = {
+export const config: MiddlewareConfig = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
@@ -8,6 +10,7 @@ export const config = {
     "/(api|trpc)(.*)",
     "/^sign-in(.*)",
     "/^sign-up(.*)",
+    "/^blog(.*)",
   ],
 };
 
