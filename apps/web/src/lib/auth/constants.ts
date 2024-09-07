0d2e05d759aeb1479497f8a2fce81c622cf1f583
Leonardo Lemos <leo.lemos.ds@icloud.com>
2024-09-07 09:49:14 -0300

    refactor: add `/pricing` and `/blog` to be prevented by auth middleware

--- apps/web/src/lib/auth/constants.ts
+++ apps/web/src/lib/auth/constants.ts
@@ -1,7 +1,15 @@
-export const PUBLIC_EXACT_ROUTES = ["/"] as const;
+/**
+ * The routes which the exact paths should be considered in the authentication middleware.
+ */
+export const PUBLIC_EXACT_ROUTES = ["/", "/pricing"] as const;
+
+/**
+ * The routes to which nesting should be considered in the authentication middleware.
+ */
 export const PUBLIC_NESTING_ROUTES = [
   "/sign-in",
   "/sign-up",
   "/blog",
+  "/developers",
   "/api/webhooks",
 ] as const;
