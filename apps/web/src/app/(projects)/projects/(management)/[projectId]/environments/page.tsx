3802243ebeecf2837e99981943a4903e79252888
Leonardo Lemos <leo.lemos.ds@icloud.com>
2024-09-09 18:25:41 -0300

    style: fix import order at projects/[projectId]/environments page

--- apps/web/src/app/(projects)/projects/(management)/[projectId]/environments/page.tsx
+++ apps/web/src/app/(projects)/projects/(management)/[projectId]/environments/page.tsx
@@ -1,9 +1,19 @@
 import type { JSX } from "react";
 
+import { Button } from "@planria/design/button";
 import { cn } from "@planria/design/css";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@planria/design/dialog";
 import { heading } from "@planria/design/typography";
 
+import { EnvironmentsProvider } from "$/domains/environments/context";
 import { EnvironmentsListView } from "$/domains/environments/list-view";
+import { NewEnvironment } from "$/domains/environments/new";
 import { fetchEnvironmentsByProjectId } from "$/server/data/projects/environments";
 
 export default async function Page({
@@ -14,13 +24,28 @@ export default async function Page({
   const environments = await fetchEnvironmentsByProjectId(params.projectId);
 
   return (
-    <div className="mx-auto container">
-      <h1 className={cn(heading({ variant: "h3" }), "mt-20 mb-10")}>
-        Environments
-      </h1>
-      <div>
-        <EnvironmentsListView environments={environments} />
+    <EnvironmentsProvider environments={environments}>
+      <div className="mx-auto container">
+        <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between">
+          <h1 className={cn(heading({ variant: "h3" }), "mt-20 mb-10")}>
+            Environments
+          </h1>
+          <Dialog variant="dismissable">
+            <DialogTrigger asChild={true}>
+              <Button>Create environment</Button>
+            </DialogTrigger>
+            <DialogContent>
+              <DialogHeader>
+                <DialogTitle>Create environment</DialogTitle>
+              </DialogHeader>
+              <NewEnvironment />
+            </DialogContent>
+          </Dialog>
+        </div>
+        <div>
+          <EnvironmentsListView />
+        </div>
       </div>
-    </div>
+    </EnvironmentsProvider>
   );
 }
