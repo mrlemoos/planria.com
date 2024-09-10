ad124b0f64f1afe2f43f3bbfe69fa6bddfeb81bc
Leonardo Lemos <leo.lemos.ds@icloud.com>
2024-09-10 20:59:15 -0300

    refactor: force `/onboarding` to be dynamic per user

--- apps/web/src/app/onboarding/page.tsx
+++ apps/web/src/app/onboarding/page.tsx
@@ -3,6 +3,8 @@ import type { JSX } from "react";
 import { Onboarding } from "$/domains/onboarding";
 import { replicateCurrentUser } from "$/server/data/users";
 
+export const dynamic = "force-dynamic";
+
 export default async function Page(): Promise<JSX.Element> {
   await replicateCurrentUser();
 
