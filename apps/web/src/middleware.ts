854ea6fcb3f652a7fbf3bf95c163dfa62f75cb7b
Leonardo Lemos <leo.lemos.ds@icloud.com>
2024-09-11 00:53:33 -0300

    refactor: redo the regexp for authentication middleware

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -6,10 +6,6 @@ export const config: MiddlewareConfig = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    // Always run for API routes
-    "/(trpc)(.*)",
-    // Skip routes nested in "/sign-in", "/sign-up", and "/blog"
-    "/((?!api|sign-in|sign-up|blog|pricing).*)",
   ],
 };
 
