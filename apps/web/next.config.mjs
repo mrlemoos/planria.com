94b110dcf8bc833e3833a1ce93c5b4dce9cde821
Leonardo Lemos <leo.lemos.ds@icloud.com>
2024-09-11 00:43:28 -0300

    build: clear up the `next.config.mjs` file for `apps/web`

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -2,15 +2,8 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-  logging: {
-    fetches: {
-      fullUrl: true,
-    },
-  },
   reactStrictMode: true,
   transpilePackages: ["@planria/design", "@planria/db", "@planria/util", "@planria/react-hooks"],
-  optimizeFonts: true,
-  output: 'standalone',
 };
 
 export default nextConfig;
