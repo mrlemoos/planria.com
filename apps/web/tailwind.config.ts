ca242c99c9e8ceb0f61773fee27127bf0cef25fb
Leonardo Lemos <leo.lemos.ds@icloud.com>
2024-09-10 22:40:36 -0300

    build: change the `build` to `dist` as tw outputs so

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -3,7 +3,7 @@ import sharedConfig from "@planria/tailwindcss";
 const config = {
   content: [
     "./src/**/*.{ts,tsx}",
-    "./node_modules/@planria/design/{src,build}/*.{ts,tsx,js,jsx}",
+    "./node_modules/@planria/design/{src,dist}/*.{ts,tsx,js,jsx}",
   ],
   presets: [sharedConfig],
 };
