249fe580640ccef50a50f4722fe224891caff94d
Leonardo Lemos <leo.lemos.ds@icloud.com>
2024-09-16 11:51:26 -0300

    chore(deps): update `turbo` to v2.1.2 via codemod

--- packages/util/src/crypto.ts
+++ packages/util/src/crypto.ts
@@ -1,4 +1,5 @@
 import { createId, isCuid } from "@paralleldrive/cuid2";
+import { createHash } from "node:crypto";
 
 export function cuid(): string {
   return createId();
@@ -6,3 +7,7 @@ export function cuid(): string {
 
 // exposes the `isCuid` function from package
 export { isCuid };
+
+export function hash(value: string): string {
+  return createHash("md5").update(value, "utf-8").digest("hex");
+}
