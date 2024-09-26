dc993b2b31d917bf836efbcd2e2feeeec99fb566
Leonardo Lemos <leo.lemos.ds@icloud.com>
2024-09-26 11:00:24 -0300

    refactor: deprecate `getBaseURL()` in favour of `getComputedBaseURL()`

--- packages/util/src/runtime.ts
+++ packages/util/src/runtime.ts
@@ -1,10 +1,27 @@
-export function isClient(
-  _window = window
-): _window is Window & typeof globalThis {
+export function isClient(): boolean {
   return typeof window !== "undefined";
 }
 
+/**
+ * @deprecated Use {@link getComputedBaseURL | `getComputedBaseURL()`} instead.
+ */
 export function getBaseURL(): string {
+  if (isClient()) {
+    return window.location.origin;
+  }
+  if (process.env.NEXT_PUBLIC_APP_URL) {
+    return process.env.NEXT_PUBLIC_APP_URL;
+  }
+  if (process.env.VERCEL_URL) {
+    return `https://${process.env.VERCEL_URL}`;
+  }
+  return `http://localhost:${process.env.PORT ?? 3000}`;
+}
+
+/**
+ * Returns the base URL for the current environment.
+ */
+export function getComputedBaseURL(): string {
   if (isClient()) {
     return window.location.origin;
   }
