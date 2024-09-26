1a60f3ee47a79d1023a4f337297d8a9c39123e59
Leonardo Lemos <leo.lemos.ds@icloud.com>
2024-09-26 10:58:35 -0300

    docs: add JSdoc assertion function

--- packages/util/src/assert.ts
+++ packages/util/src/assert.ts
@@ -1,9 +1,15 @@
+import { isNonNullable } from "./primitives";
+
 export class AssertionError extends Error {
   constructor(message: string) {
     super(message);
   }
 }
 
+/**
+ * Asserts the given condition is true or throws an error with the provided message
+ * given that the condition is false.
+ */
 export function assert<U, E>(
   condition: U,
   error: E
@@ -11,8 +17,12 @@ export function assert<U, E>(
   if (!condition) {
     if (typeof error === "string") {
       throw new AssertionError(error);
-    } else {
+    } else if (error instanceof Error || isNonNullable(error)) {
       throw error;
+    } else if (typeof error === "function") {
+      throw error();
+    } else {
+      throw new AssertionError("Assertion failed");
     }
   }
 }
