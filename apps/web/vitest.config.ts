9d2db090345262e62bde81c271a825dd5b38d3fc
Leonardo Lemos <leo.lemos.ds@icloud.com>
2024-09-27 14:47:39 -0300

    test(config): add react plugin to vitest environment

--- apps/web/vitest.config.ts
+++ apps/web/vitest.config.ts
@@ -1,7 +1,9 @@
+import react from "@vitejs/plugin-react";
 import path from "node:path";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
+  plugins: [react()],
   test: {
     globals: true,
     environment: "happy-dom",
