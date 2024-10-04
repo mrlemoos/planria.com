a627bada13d56e269b73259085b375643fdfe012
Leonardo Lemos <leo.lemos.ds@icloud.com>
2024-10-03 23:24:00 -0300

    refactor(design): make badge component slottable

--- packages/design/src/badge.tsx
+++ packages/design/src/badge.tsx
@@ -1,6 +1,9 @@
+"use client";
+
 import type { HTMLAttributes, JSX } from "react";
 
 import { cn, stylesheet, type VariantProps } from "./css";
+import { Slot } from "./slot";
 
 export const createBadgeVariants = stylesheet.create({
   base: "inline-flex items-center rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2",
@@ -15,6 +18,8 @@ export const createBadgeVariants = stylesheet.create({
       outline: "text-foreground",
       success:
         "border-transparent bg-success-background text-success-foreground",
+      notice:
+        "px-4 py-2 font-semibold text-base text-white bg-zinc-50 dark:bg-zinc-950 border-2 border-zinc-100 dark:border-zinc-900 bg-blur-sm",
     },
   },
   defaultVariants: {
@@ -24,17 +29,25 @@ export const createBadgeVariants = stylesheet.create({
 
 export interface BadgeProps
   extends HTMLAttributes<HTMLDivElement>,
-    VariantProps<typeof createBadgeVariants> {}
+    VariantProps<typeof createBadgeVariants> {
+  asChild?: boolean;
+}
 
 export function Badge({
   className,
   variant,
+  children,
+  asChild = false,
   ...props
 }: BadgeProps): JSX.Element {
+  const RootElement = asChild ? Slot : "div";
+
   return (
-    <div
+    <RootElement
       className={cn(createBadgeVariants({ variant }), className)}
       {...props}
-    />
+    >
+      {children}
+    </RootElement>
   );
 }
