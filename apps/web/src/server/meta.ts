a913c3f373ed27997b4f51daa04c43c055d0f7ac
Leonardo Lemos <leo.lemos.ds@icloud.com>
2024-11-12 23:35:50 -0300

    refactor: add license + git repo url in `meta.ts`

--- apps/web/src/server/meta.ts
+++ apps/web/src/server/meta.ts
@@ -9,6 +9,9 @@ export const APP_URL = `https://${DOMAIN}` as const;
 
 export const APP_VERSION = `${packageJSON.version}` as const;
 
+export const GIT_REPOSITORY_URL =
+  "https://github.com/mrlemoos/planria.com" as const;
+
 export const PRODUCT_DESCRIPTION = `${packageJSON.description}` as const;
 
 export const CONTACT_EMAIL = `contact@${DOMAIN}` as const;
@@ -17,3 +20,10 @@ export const SUPPORT_EMAIL = `support@${DOMAIN}` as const;
 
 export const PURCHASES_EMAIL = `purchases@${DOMAIN}` as const;
 
+export const SOFTWARE_LICENSE = `${packageJSON.license}` as const;
+
+export const SOFTWARE_LICENSE_URL =
+  "https://github.com/planria/planria/blob/main/LICENSE" as const;
+
+export const FULL_SOFTWARE_LICENSE_URL =
+  "https://apache.org/licenses/LICENSE-2.0" as const;
