2f6cd3c21a802cc1a81675cccf37603c4a042d99
Leonardo Lemos <leo.lemos.ds@icloud.com>
2024-11-12 23:34:15 -0300

    build(next-config): configure to transpile mdx files

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,9 +1,14 @@
+import createMDX from '@next/mdx';
+
 /** 
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
   reactStrictMode: true,
   transpilePackages: ["@planria/design", "@planria/db", "@planria/util", "@planria/react-hooks"],
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
 };
 
-export default nextConfig;
+const withMDX = createMDX({});
+
+export default withMDX(nextConfig);
