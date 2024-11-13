3f145eff9cb98230edc4b731ac09cc40b653a8ad
Leonardo Lemos <leo.lemos.ds@icloud.com>
2024-11-12 23:34:27 -0300

    refactor: add hard-coded license

---
+++ apps/web/src/license.ts
@@ -0,0 +1,23 @@
+const LICENSE_RAW = `
+Copyright 2024 Planria Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+`;
+
+/**
+ * The read raw-license content from the `LICENSE.md` found inside the root folder
+ * of the project. This is expected to have access to the license text and should
+ * only be used for displaying the license text on the website. DO NOT use it
+ * for any other purpose.
+ */
+export const license = LICENSE_RAW;
